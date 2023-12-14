0edeb105dc16448ae2c320e5fae2820bae119e83
Mariohjr94 <mariohjr94@gmail.com>
2023-12-14 10:57:23 -0500

    react intro practice

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -3,13 +3,38 @@ import "./App.css";
 import { puppyList } from "./data.js";
 
 function App() {
-  console.log(puppyList);
+  // console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  function handleClick() {}
+
   return (
     <div className="App">
+      <h1>Puppy Pals</h1>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+
+      {featPupId && (
+        <div className="container">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
