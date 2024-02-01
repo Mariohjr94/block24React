673a87a5b5a27b41081794a9e44d03ea6d7a6d33
Mariohjr94 <mariohjr94@gmail.com>
2024-01-31 20:49:59 -0500

    block31

--- block-JS-practice/index.js
+++ block-JS-practice/index.js
@@ -1,31 +1,14 @@
-Create a fetch request to https://api.chucknorris.io/jokes/random and console.log() the data that is returned.
+const myReduce = (arr, callback, startingValue) => {
+  let accumulator = startingValue;
 
-async const fetchFunction = () => { // adding a try catch...
-    const response = await fetch(" https://api.chucknorris.io/jokes/random");
-    const Json = await response.json();
-    console.log(Json);
-}
-
-
-
-function disemvowel(string) {
-  // your code here...
-  let newStr = '';
-  let vowels = 'aeiouAEIOU';
-  for (let i = 0; i < string.length; i++) {
-    const currentLetter = string[i];
-    if (!vowels.includes(currentLetter)){
-        newStr.push(currentLetter); // newStr += currentLetter, push is an array method
-    }
+  for (let i = 0; i < arr.length; i++) {
+    accumulator = callback(accumulator, arr[i]);
   }
-  return newStr;
-}
 
+  return accumulator;
+};
 
+const nums = [1, 2, 3, 4, 5];
+const result = myReduce(nums, (a, b) => a + b, 0);
 
-
-//Uncomment the lines below to test your function:
-
-// console.log(disemvowel('Fullstack')); // => 'Fllstck'
-// console.log(disemvowel('BANANA')); // => 'BNN'
-// console.log(disemvowel('hello world')); // => 'hll wrld'
+console.log(result); // => 15
